42840b3e820425713fedaa9e04ed2d373deab37e
octapf <frangipani.octavio@gmail.com>
2024-01-26 06:54:00 +0100

    add config environment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import { UserRoute } from './routes/user.route'
+import { ConfigServer } from './config/config'
 
-class Server {
+class Server extends ConfigServer {
 	public app: express.Application = express()
-	private port: number = 5000
+	private port: number = this.getNumberEnv('PORT') || 8000
 
 	constructor() {
+		super()
 		this.app.use(express.json())
 		this.app.use(express.urlencoded({ extended: true }))
 		this.app.use(morgan('dev'))
